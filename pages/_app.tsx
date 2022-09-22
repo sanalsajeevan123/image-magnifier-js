ffe2ac50e9f08f77bcb1695e37d3c31cfd1ed7e2
sanalsajeevan123 <sanalsajeevan123@gmail.com>
2022-09-22 13:16:47 +0530

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/magnifier.css'
+import '../styles/hover.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
